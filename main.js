b8d6d64f4b533bb9dc112220561ac1f815fddfc5
ammarhanyy <163277929+ammarhanyy@users.noreply.github.com>
2024-05-14 13:21:47 +0300

    First Try

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 
-
-
-
   function AddingToCart(){
-    DoneOb.innerHTML +=this.parentElement;
+    function addToCart(divId) {
+      // Find the div element to add to cart
+      var itemToAdd = document.getElementById("product");
+      
+      // Clone the div element
+      var clonedItem = itemToAdd.cloneNode(true);
+      
+      // Append the cloned div to the shopping cart container
+      var cart = document.getElementById('Addeditems');
+      
+      cart.appendChild(clonedItem);
+  }
+    
+
+
   }
 
 
